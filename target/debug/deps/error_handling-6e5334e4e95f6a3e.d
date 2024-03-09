57bd2a59b77d590a74c930a712efc1797239a721
Saksham Gupta <sakshamgupta.ai@gmail.com>
2024-03-09 16:51:48 +0530

    first commit

---
+++ target/debug/deps/error_handling-6e5334e4e95f6a3e.d
@@ -0,0 +1,5 @@
+/home/saksham/Documents/Development/Rust/error_handling_match/target/debug/deps/error_handling-6e5334e4e95f6a3e: src/main.rs
+
+/home/saksham/Documents/Development/Rust/error_handling_match/target/debug/deps/error_handling-6e5334e4e95f6a3e.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/error_handling.d
@@ -0,0 +1,1 @@
+/home/saksham/Documents/Development/Rust/error_handling_match/target/debug/error_handling: /home/saksham/Documents/Development/Rust/error_handling_match/src/main.rs
